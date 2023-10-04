104fe0bd8d0d3ed46d3c5d137c702eb8fb52aa6e
marinos.petratos <marinos.petratos@leaseplan.com>
2023-10-04 12:26:50 +0200

    first commit

---
+++ src/test/java/starter/utils/AddMoonVideoExternalLink.java
@@ -0,0 +1,25 @@
+package starter.utils;
+
+import net.serenitybdd.core.Serenity;
+import net.serenitybdd.core.webdriver.enhancers.AfterAWebdriverScenario;
+
+
+import net.thucydides.model.domain.ExternalLink;
+import net.thucydides.model.domain.TestOutcome;
+import net.thucydides.model.util.EnvironmentVariables;
+import org.openqa.selenium.WebDriver;
+
+public class AddMoonVideoExternalLink implements AfterAWebdriverScenario {
+
+    private static final String URL = "https://www.google.com";
+
+    @Override
+    public void apply(EnvironmentVariables environmentVariables, TestOutcome testOutcome, WebDriver driver) {
+        String sessionId = Serenity.getCurrentSessionID();
+        testOutcome.setLink(
+                new ExternalLink(
+                        String.format("%s/%s", URL, sessionId), "A url"
+                )
+        );
+    }
+}
